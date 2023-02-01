8a20b8e7db27f4a2b88f4641b128c82ca48b8c5a
User01 <romansochirca30@gmail.com>
2023-02-01 19:21:52 +0200

    Fix MemoriesPage

--- src/components/BaseLayout.vue
+++ src/components/BaseLayout.vue
@@ -3,6 +3,9 @@
 
     <ion-header>
       <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-back-button :default-href="pageDefaultBackLink"></ion-back-button>
+        </ion-buttons>
         <ion-title> {{ pageTitle }}</ion-title>
       </ion-toolbar>
     </ion-header>
@@ -21,16 +24,20 @@ import {
   IonToolbar,
   IonTitle,
   IonContent,
+  IonBackButton,
+  IonButtons,
 } from '@ionic/vue';
 
 export default {
-  props: ['pageTitle'],
+  props: ["pageTitle", "pageDefaultBackLink"],
   components: {
     IonPage,
     IonHeader,
     IonToolbar,
     IonTitle,
     IonContent,
+    IonBackButton,
+    IonButtons,
   }
 }
 </script>
--- src/pages/MemoriesPage.vue
+++ src/pages/MemoriesPage.vue
@@ -1,10 +1,18 @@
 <template>
   <base-layout pageTitle="All Memories">
     <ion-list>
-      <ion-item router-link="/memories/1">Roman</ion-item>
-      <ion-item>Max</ion-item>
-      <ion-item>Alexadru</ion-item>
-      <ion-item>Daniel</ion-item>
+
+      <ion-item
+        v-for="memory in memories"
+        :router-link="`/memories/${memory.id}`"
+        :key="memory.id"
+      >
+        <ion-thumbnail slot="start">
+          <ion-img :src="memory.image" :alt="memory.title"></ion-img>
+        </ion-thumbnail>
+        <ion-label>{{ memory.title }}</ion-label>
+      </ion-item>
+
     </ion-list>
   </base-layout>
 </template>
@@ -13,13 +21,43 @@
 import {
   IonList,
   IonItem,
+  IonImg,
+  IonThumbnail,
+  IonLabel,
 } from '@ionic/vue';
 
 export default {
   components: {
     IonList,
     IonItem,
-  }
+    IonImg,
+    IonThumbnail,
+    IonLabel,
+  },
+  data() {
+    return {
+      memories: [
+        {
+          id: 1,
+          image: "/assets/icon/icon.png",
+          title: "Title nr.1",
+          description: "This is a short description nr.1"
+        },
+        {
+          id: 2,
+          image: "/assets/icon/icon.png",
+          title: "Title nr.2",
+          description: "This is a short description nr.2"
+        },
+        {
+          id: 3,
+          image: "/assets/icon/icon.png",
+          title: "Title nr.3",
+          description: "This is a short description nr.3"
+        },
+      ]
+    };
+  },
 }
 </script>
 
