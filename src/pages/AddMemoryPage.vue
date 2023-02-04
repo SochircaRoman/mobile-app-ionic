98d920bbd0c97b733f014449f6499410e4ce73c2
User01 <romansochirca30@gmail.com>
2023-02-04 16:46:52 +0200

    Make decomposition for MemoryForm

--- src/pages/AddMemoryPage.vue
+++ src/pages/AddMemoryPage.vue
@@ -1,39 +1,20 @@
 <template>
-  <base-layout
-    page-title="Add a Memory"
-    page-default-backLink="/memories"
-  >
-    <form class="ion-padding">
-      <ion-list>
-        <ion-item>
-          <ion-label position="floating">Title</ion-label>
-          <ion-input type="text" required />
-        </ion-item>
-        <ion-item>
-          <ion-label position="floating">Image URL</ion-label>
-          <ion-input type="url" required />
-        </ion-item>
-        <ion-item>
-          <ion-label position="floating">Description</ion-label>
-          <ion-textarea rows="5"></ion-textarea>
-        </ion-item>
-        <ion-button expand="block" fill="">Save</ion-button>
-      </ion-list>
-    </form>
+  <base-layout page-title="Add a Memory" page-default-backLink="/memories">
+    <create-memory-form></create-memory-form>
   </base-layout>
 </template>
 
 <script>
-import { IonList, IonItem, IonLabel, IonInput, IonTextarea, IonButton } from '@ionic/vue';
+import CreateMemoryForm from '../components/memories/CreateMemoryForm.vue';
 
 export default {
   components: {
-    IonList,
-    IonItem,
-    IonLabel,
-    IonInput,
-    IonTextarea,
-    IonButton,
+    CreateMemoryForm,
+  },
+  methods: {
+    saveMemory(memoryData) {
+      
+    }
   }
 };
 </script>
