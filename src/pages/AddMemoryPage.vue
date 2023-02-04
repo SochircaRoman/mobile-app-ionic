b7dc2c80e6e5dbc92eb564148b05b40ca30fc366
User01 <romansochirca30@gmail.com>
2023-02-04 16:19:45 +0200

    Add AddMemory Btn

--- src/pages/AddMemoryPage.vue
+++ src/pages/AddMemoryPage.vue
@@ -1,13 +1,14 @@
 <template>
-  <div>
-
-  </div>
+  <base-layout
+    page-title="Add a Memory"
+    page-default-backLink="/memories"
+  >
+    <h2>Add a memory</h2>
+  </base-layout>
 </template>
 
 <script>
-export default {
-  
-}
+export default {};
 </script>
 
 <style>
