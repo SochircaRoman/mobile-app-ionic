91de4979fd07de6fe41c83f3bd697cb679501029
User01 <romansochirca30@gmail.com>
2023-03-09 13:51:10 +0200

    Add auth.module

--- src/store/index.js
+++ src/store/index.js
@@ -1,57 +1,10 @@
 import { createStore } from "vuex";
+import { auth } from "./auth.module";
 
 const store = createStore({
-  state() {
-    return {
-      memories: [
-        {
-          id: 1,
-          image: "/assets/icon/icon.png",
-          title: "Title nr.1",
-          description: "This is a short description nr.1"
-        },
-        {
-          id: 2,
-          image: "/assets/icon/icon.png",
-          title: "Title nr.2",
-          description: "This is a short description nr.2"
-        },
-        {
-          id: 3,
-          image: "/assets/icon/icon.png",
-          title: "Title nr.3",
-          description: "This is a short description nr.3"
-        },
-      ]
-    };
+  modules: {
+    auth,
   },
-  mutations: {
-    addMemory(state, memoryData) {
-      const newMemory = {
-        id: new Date().toISOString(),
-        title: memoryData.title,
-        image: memoryData.image,
-        description: memoryData.description
-      };
-
-      state.memories.unshift(newMemory);
-    }
-  },
-  actions: {
-    addMemory(context, memoryData) {
-      context.commit('addMemory', memoryData); 
-    }
-  },
-  getters: {
-    memories(state) {
-      return state.memories;
-    },
-    memory(state) {
-      return (memoryId) => {
-        return state.memories.find((memory) => memory.id == memoryId);
-      };
-    }
-  }
 });
 
 export default store;
